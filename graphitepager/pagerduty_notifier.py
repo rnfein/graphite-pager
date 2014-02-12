81214c0acc97b4bcbb82842c0b33d447e950033e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-02-12 17:29:20 -0500

    Allow overriding the pagerduty service_key for a given alert

--- graphitepager/pagerduty_notifier.py
+++ graphitepager/pagerduty_notifier.py
@@ -8,6 +8,11 @@ class PagerdutyNotifier(object):
         self._storage = storage
 
     def notify(self, alert, alert_key, level, description, html_description):
+        service_key = None
+        if alert.get('pagerduty_key', None) is not None:
+            service_key = self._client.service_key
+            self._client.service_key = alert.get('pagerduty_key', None)
+
         incident_key = self._storage.get_incident_key_for_alert_key(alert_key)
         if level != Level.NOMINAL:
             description = str(description)
@@ -16,3 +21,6 @@ class PagerdutyNotifier(object):
         elif incident_key is not None:
             self._client.resolve(incident_key=incident_key)
             self._storage.remove_incident_for_alert_key(alert_key)
+
+        if service_key:
+            self._client.service_key = service_key
